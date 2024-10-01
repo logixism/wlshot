ba13cdff6b7cd295156302dce593db7fafe59dc9
logix <logixism@pm.me>
2024-10-01 19:50:30 +0300

    fix notification if the image requires conversion

--- wlshot.fish
+++ wlshot.fish
@@ -32,11 +32,11 @@ function capture
         magick $IMAGE.$FORMAT $IMAGE.$CONVERT_TO # if specified, convert the image to the required format
         wl-copy --type image/png < "$IMAGE.$CONVERT_TO"
         rm -f "$IMAGE.$FORMAT"
+        notify-send "Screenshot saved" "Image saved and copied to the clipboard." -t "2000" -i "$IMAGE.$CONVERT_TO" -a wlshot
     else
         wl-copy --type image/png < "$IMAGE.$FORMAT"
+        notify-send "Screenshot saved" "Image saved and copied to the clipboard." -t "2000" -i "$IMAGE.$FORMAT" -a wlshot
     end
-
-    notify-send "Screenshot saved" "Image saved and copied to the clipboard." -t "2000" -i "$IMAGE.$FORMAT" -a wlshot
 end
 
 set OUTPUT_FOLDER ~/pictures/screenshots
